8199869509aebadd141869ac313f342b18309591
Lazox12 <tomik.lazar@gmail.com>
2025-08-20 16:57:38 +0200

    test

---
+++ src/app/window/window-home/window-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-window-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './window-home.component.html',
+  styleUrl: './window-home.component.css'
+})
+export class WindowHomeComponent {
+
+}
